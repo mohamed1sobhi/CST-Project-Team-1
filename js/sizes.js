c5d0d93518c53422139adbc1e4d1bfbc639e059a
AminaMa7moud <121107471+AminaMa7moud@users.noreply.github.com>
2025-01-10 06:12:16 +0200

    style changes

--- js/sizes.js
+++ js/sizes.js
@@ -98,3 +98,169 @@ colors.forEach(color => {
         }
     });
 });
+
+
+
+
+
+// Add counter functionality to the input fields
+$(document).ready(function () {
+  $(".btn-minus").click(function () {
+    let input = $(this).closest(".input-group").find("input");
+    let value = parseInt(input.val());
+    if (value > 1) {
+      input.val(value - 1);
+    }
+  });
+  $(".btn-plus").click(function () {
+    let input = $(this).closest(".input-group").find("input");
+    let value = parseInt(input.val());
+    input.val(value + 1);
+  });
+});
+
+// Add rating functionality to the stars
+$(document).ready(function () {
+  $("#rating i").click(function () {
+    var index = $(this).index();
+    $(this).siblings().removeClass("fas").addClass("far");
+    $(this).prevAll().removeClass("far").addClass("fas");
+    $(this).removeClass("far").addClass("fas");
+  });
+});
+
+// Add review functionality to the form
+$("#reviewForm").submit(function (e) {
+    e.preventDefault();
+    var name = $("#nameval").val();
+    var review = $("#review").val();
+    var rating = $("#rating i.fas").length;
+    if (name == "" || rating === 0 || review =="") {
+        //alert("Please fill all required fields.");
+        Swal.fire({
+          title: "Oops!",
+          text: "Please fill all required fields.",
+          icon: "warning",
+          confirmButtonText: "OK",
+        });
+    } else {
+      Swal.fire({
+        title: `Thank you ${name} for review`,
+        text: `your rating is ${rating} out of 5.`,
+        icon: "success",
+        confirmButtonText: "OK",
+      });
+    }
+});
+
+
+// Add product to cart functionality
+$(document).on("click", "#triggerButton", async function () {
+  const productName = $("h3.text-uppercase").text().trim();
+  const productPriceText = $("h3.font-weight-semi-bold.mb-4.text-secondary.mt-1").text().trim();
+  const productImage = $("#product-carousel .carousel-item.active img").attr("src");
+  const productPrice = parseFloat(productPriceText.replace("$", "").trim());
+  const confirmAction = await Swal.fire({
+    title: "Are you sure?",
+    text: `You want to add "${productName}" with price $${productPrice} to the cart?`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes, add it!",
+    cancelButtonText: "No, cancel!",
+  });
+  // If user confirms the action
+  if (confirmAction.isConfirmed) {
+    const product = {
+      name: productName,
+      price: productPrice,
+      image: productImage,
+      quantity: 1,
+    };
+
+    // Get cart items from localStorage
+    let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const existingProductIndex = cartItems.findIndex(
+      (item) => item.name === product.name
+      );
+      
+
+    if (existingProductIndex === -1) {
+      // If product is not already in the cart, add it
+      cartItems.push(product);
+        Swal.fire({
+          icon: "success",
+          title: `"${productName}" added to the cart.`,
+          showConfirmButton: false,
+          timer: 1000,
+        });
+
+    }
+    else {
+        Swal.fire({
+          icon: "info",
+          title: `"${productName}" is already in the cart.`,
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      }
+      
+    // Save updated cartItems to localStorage only if product was added
+    if (existingProductIndex === -1) {
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    }
+  }
+});
+
+
+// Add You may AlsoLike products to wishlist functionality
+$(document).on("click", ".fa-shopping-cart", async function () {
+  const productItem = $(this).closest(".product-item");
+  const productName = productItem.find("a.h3.text-decoration-none.text-warning").text().trim();
+    const productPriceText = productItem.find("h5").text().trim();
+  const productImage = productItem.find(".product-img img").attr("src");
+  const productPrice = parseFloat(productPriceText.replace("$", "").trim());
+
+  const confirmAction = await Swal.fire({
+    title: "Are you sure?",
+    text: `You want to add "${productName}" with price $${productPrice} to the cart?`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes, add it!",
+    cancelButtonText: "No, cancel!",
+  });
+
+  if (confirmAction.isConfirmed) {
+    const product = {
+      name: productName,
+      price: productPrice,
+      image: productImage,
+      quantity: 1,
+    };
+
+    let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const existingProductIndex = cartItems.findIndex(
+      (item) => item.name === product.name
+    );
+
+    if (existingProductIndex > -1) {
+      Swal.fire({
+        icon: "info",
+        title: `"${productName}" is already in the cart.`,
+        showConfirmButton: false,
+        timer: 1000,
+      });
+    } else {
+      cartItems.push(product);
+      Swal.fire({
+        icon: "success",
+        title: `"${productName}" added to the cart.`,
+        showConfirmButton: false,
+        timer: 1000,
+      });
+    }
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }
+});
+
+
+
