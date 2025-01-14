670cf4a44d9cf758c06e488d5fc80a41233dc885
Khaledgaafar <khaledgafaar@gmail.com>
2025-01-15 00:24:33 +0200

    khaledcommit

--- js/stock.js
+++ js/stock.js
@@ -1,10 +1,11 @@
 class Products {
   static id = 1; // Static variable shared across all instances, starts at 1
-  constructor(name, price, cost = 100, quantity = 5, seller_id, img, active) {
+  constructor(name, price, cost = 100,desc, quantity = 5, seller_id, img, active) {
     this.productid = Products.id++; // Assigns current id then increments it
     this.name = name;
     this.price = price;
     this.cost = cost;
+    this.description = desc;
     this.quantity = quantity;
     this.seller = seller_id;
     this.img = img;
