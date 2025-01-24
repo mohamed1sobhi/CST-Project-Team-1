7c0c42f63bcbc9dac2bef2d2bde9cac5a141816e
Mohamed Sobhi Anwar Ali <mohamed.sobhi.anwar@gmail.com>
2025-01-24 14:27:57 +0200

    latest update

--- js/charts.js
+++ js/charts.js
@@ -1,25 +1,25 @@
 import { getProducts } from "./productstore.js";
-window.onload = function() { 
-    let products = getProducts();
-    let totalProducts = Object.keys(products).length;
+window.onload = function () {
+  let products = getProducts();
+  let totalProducts = Object.keys(products).length;
 
-    if (localStorage.getItem('count')) { 
-        let count = parseInt(localStorage.getItem('count'));
-        let total = document.getElementById('total');
-        total.innerText = `${totalProducts}`;
-        let number = document.getElementById('counter'); 
-        number.innerText = "/"+count; 
-        let progress = document.getElementById("progress");
-        progress.style.width = (totalProducts / count * 100) + '%'; // Changed this line
-    } 
+  if (localStorage.getItem("count")) {
+    let count = parseInt(localStorage.getItem("count"));
+    let total = document.getElementById("total");
+    total.innerText = `${totalProducts}`;
+    let number = document.getElementById("counter");
+    number.innerText = "/" + count;
+    let progress = document.getElementById("progress");
+    progress.style.width = (totalProducts / count) * 100 + "%";
+  }
 
-    if (localStorage.getItem('countofpurchace')) { 
-        let countofpurchace = parseInt(localStorage.getItem('countofpurchace'));
-        let total1 = document.getElementById('total1');
-        total1.innerText = `${totalProducts}`;
-        let number = document.getElementById('counter1'); 
-        number.innerText = "/"+countofpurchace; 
-        let progress = document.getElementById("progress1");
-        progress.style.width = (totalProducts / countofpurchace * 100) + '%'; // Changed this line
-    } 
+  if (localStorage.getItem("countofpurchace")) {
+    let countofpurchace = parseInt(localStorage.getItem("countofpurchace"));
+    let total1 = document.getElementById("total1");
+    total1.innerText = `${totalProducts}`;
+    let number = document.getElementById("counter1");
+    number.innerText = "/" + countofpurchace;
+    let progress = document.getElementById("progress1");
+    progress.style.width = (totalProducts / countofpurchace) * 100 + "%";
+  }
 };
